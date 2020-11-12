43b38a33560464066b3d57dcea7dafe6067fd925
James Bell <james@bellstone.ca>
2020-11-12 15:58:29 -0700

    Improve Stringer for Tag type

--- tag.go
+++ tag.go
@@ -5,6 +5,7 @@ package plc
 */
 import "C"
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -31,15 +32,17 @@ func (tag *Tag) addDimension(dim int) {
 }
 
 func (tag Tag) String() string {
+	name := fmt.Sprintf("%s{%04X}", tag.name, int(tag.tagType))
+
 	if len(tag.dimensions) == 0 {
-		return tag.name
+		return name
 	}
 
 	strs := make([]string, len(tag.dimensions))
 	for i, v := range tag.dimensions {
 		strs[i] = strconv.Itoa(v)
 	}
-	return tag.name + "[" + strings.Join(strs, ",") + "]"
+	return name + "[" + strings.Join(strs, ",") + "]"
 }
 
 func (tag Tag) ElemCount() int {
