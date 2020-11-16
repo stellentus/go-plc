f3550c6e9cc73b5441e4ccd5968f16c5b36943b5
James Bell <james@bellstone.ca>
2020-11-16 12:39:43 -0700

    Add missing plc lock/unlock
    * The stub doesn't implement them

--- libplctag_stub.go
+++ libplctag_stub.go
@@ -5,6 +5,16 @@ package plc
 /*
 #cgo LDFLAGS: ./libplctag_stub.a
 #include "./libplctag.h"
+
+LIB_EXPORT int plc_tag_lock(int32_t tag)
+{
+	return PLCTAG_STATUS_OK;
+}
+LIB_EXPORT int plc_tag_unlock(int32_t tag)
+{
+	return PLCTAG_STATUS_OK;
+}
+
 */
 import "C"
 
