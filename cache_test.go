bef89c0fa65bfb6fb0b57968541361220e6845d2
James Bell <james@bellstone.ca>
2020-11-20 14:59:19 -0700

    Add extra test cases

--- cache_test.go
+++ cache_test.go
@@ -17,7 +17,7 @@ func TestNewCache(t *testing.T) {
 	assert.NotNil(t, cache, "NewCache should return a non-nil object")
 }
 
-func TestCachePassThroughInt(t *testing.T) {
+func TestCachePassthroughInt(t *testing.T) {
 	cache, deviceFake := newCacheForTesting()
 	deviceFake[testTagName] = 7
 
@@ -27,17 +27,98 @@ func TestCachePassThroughInt(t *testing.T) {
 	assert.Equal(t, 7, actual)
 }
 
-func TestCacheCachesInt(t *testing.T) {
+func TestCachePassthroughTwiceInt(t *testing.T) {
 	cache, deviceFake := newCacheForTesting()
 	deviceFake[testTagName] = 7
 
 	var unused int
 	err := cache.ReadTag(testTagName, &unused)
 	require.NoError(t, err)
-	unused++ // increment the value to ensure we aren't just returning a pointer
+
+	deviceFake[testTagName] = 85
+
+	var actual int
+	err = cache.ReadTag(testTagName, &actual)
+	assert.NoError(t, err)
+	assert.Equal(t, 85, actual)
+}
+
+func TestCacheInt(t *testing.T) {
+	cache, deviceFake := newCacheForTesting()
+	deviceFake[testTagName] = 7
+
+	var unused int
+	err := cache.ReadTag(testTagName, &unused)
+	require.NoError(t, err)
+
+	// Mess up both values to ensure we're not just getting a pointer
+	unused++
+	deviceFake[testTagName] = 13
 
 	var actual int
 	err = cache.ReadCachedTag(testTagName, &actual)
+	assert.NoError(t, err)
+	assert.Equal(t, 7, actual)
+}
+
+func TestCacheAfterUpdateInt(t *testing.T) {
+	cache, deviceFake := newCacheForTesting()
+	deviceFake[testTagName] = 7
+
+	// Update the cache with a value we'll never look at
+	var unused int
+	err := cache.ReadTag(testTagName, &unused)
+	require.NoError(t, err)
+
+	// Set the value to 85 and read it, expecting this to end up in the cache
+	deviceFake[testTagName] = 85
+	err = cache.ReadTag(testTagName, &unused)
+	require.NoError(t, err)
+
+	var actual int
+	err = cache.ReadCachedTag(testTagName, &actual)
+	assert.NoError(t, err)
+	assert.Equal(t, 85, actual)
+}
+
+const secondTestTagName = "secondTag"
+
+func TestCacheSecondTag(t *testing.T) {
+	cache, deviceFake := newCacheForTesting()
+	deviceFake[testTagName] = 7
+	deviceFake[secondTestTagName] = 99
+
+	var unused int
+	err := cache.ReadTag(testTagName, &unused)
+	require.NoError(t, err)
+	err = cache.ReadTag(secondTestTagName, &unused)
+	require.NoError(t, err)
+
+	// Ensure BOTH are read correctly
+	var actual int
+	err = cache.ReadCachedTag(testTagName, &actual)
+	assert.NoError(t, err)
+	assert.Equal(t, 7, actual)
+	err = cache.ReadCachedTag(secondTestTagName, &actual)
+	assert.NoError(t, err)
+	assert.Equal(t, 99, actual)
+}
+
+func TestCacheReader(t *testing.T) {
+	cache, deviceFake := newCacheForTesting()
+	cacheReader := cache.CacheReader()
+	deviceFake[testTagName] = 7
+
+	var unused int
+	err := cache.ReadTag(testTagName, &unused)
+	require.NoError(t, err)
+
+	// Mess up both values to ensure we're not just getting a pointer
+	unused++
+	deviceFake[testTagName] = 13
+
+	var actual int
+	err = cacheReader.ReadTag(testTagName, &actual)
 	assert.NoError(t, err)
 	assert.Equal(t, 7, actual)
 }
