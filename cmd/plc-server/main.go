d172a14eeadae779ce180cdfedc3281bf6a24d7f
James Bell <james@bellstone.ca>
2020-11-23 12:33:39 -0700

    Add a refresher to plc-server

--- cmd/plc-server/main.go
+++ cmd/plc-server/main.go
@@ -4,14 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/stellentus/go-plc"
 	"github.com/stellentus/go-plc/example"
 )
 
 var (
-	plcAddr    = flag.String("plc-address", "192.168.1.176", "Hostname or IP address of the PLC")
-	httpAddr   = flag.String("http", ":8784", "Port for http server to listen to")
-	numWorkers = flag.Int("workers", 1, "Number of worker threads talking to libplctag")
+	plcAddr         = flag.String("plc-address", "192.168.1.176", "Hostname or IP address of the PLC")
+	httpAddr        = flag.String("http", ":8784", "Port for http server to listen to")
+	numWorkers      = flag.Int("workers", 1, "Number of worker threads talking to libplctag")
+	refreshDuration = flag.Duration("refresh", time.Second, "Refresh period")
 )
 
 var knownTags = map[string]interface{}{
@@ -31,11 +34,12 @@ func main() {
 	flag.Parse()
 
 	device, err := example.NewDevice(example.Config{
-		Workers:          *numWorkers,
-		PrintReadDebug:   true,
-		PrintWriteDebug:  true,
-		DebugFunc:        fmt.Printf,
-		DeviceConnection: map[string]string{"gateway": *plcAddr},
+		Workers:           *numWorkers,
+		PrintReadDebug:    true,
+		PrintWriteDebug:   true,
+		DebugFunc:         fmt.Printf,
+		DeviceConnection:  map[string]string{"gateway": *plcAddr},
+		RefresherDuration: *refreshDuration,
 	})
 	panicIfError(err, "Could not create test PLC!")
 	defer func() {
@@ -45,10 +49,23 @@ func main() {
 		}
 	}()
 
+	initializeRefresher(device.Refresher())
+
 	http.Handle("/tags/raw", RawTagsHandler{device, knownTags})
 	fmt.Printf("Making PLC '%s' available at '%s'\n", *plcAddr, *httpAddr)
 	err = http.ListenAndServe(*httpAddr, nil)
 	panicIfError(err, "Could not start http server!")
+}
+
+func initializeRefresher(rd plc.Reader) error {
+	for tag, v := range knownTags {
+		val := copy(v)
+		err := rd.ReadTag(tag, val)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func panicIfError(err error, reason string) {
