6681ebb35bdb8e4fd723fc80ce20cba7ed3564d6
Matthias Stone <matthias@bellstone.ca>
2020-11-23 10:02:17 -0700

    Basic HTTP REST API
    WTP-14

---
+++ cmd/plc-server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/stellentus/go-plc"
+)
+
+var (
+	plcAddr  = flag.String("plc-address", "192.168.1.176", "Hostname or IP address of the PLC")
+	path     = flag.String("path", "1,0", "Path to the PLC at the provided host or IP")
+	httpAddr = flag.String("http", ":8784", "Port for http server to listen to")
+)
+
+var knownTags = map[string]interface{}{
+	"DUMMY_AQUA_DATA_0": uint16(0),
+	"DUMMY_AQUA_DATA_1": uint16(0),
+	"DUMMY_AQUA_DATA_2": uint16(0),
+	"DUMMY_AQUA_DATA_3": uint16(0),
+	"DUMMY_AQUA_DATA_4": uint16(0),
+	"DUMMY_AQUA_DATA_5": uint16(0),
+	"DUMMY_AQUA_DATA_6": uint16(0),
+	"DUMMY_AQUA_DATA_7": uint16(0),
+	"DUMMY_AQUA_DATA_8": uint16(0),
+	"DUMMY_AQUA_DATA_9": uint16(0),
+}
+
+func main() {
+	flag.Parse()
+
+	connectionInfo := fmt.Sprintf("protocol=ab_eip&gateway=%s&path=%s&cpu=controllogix", *plcAddr, *path)
+	timeout := 5 * time.Second
+	device, err := plc.NewDevice(connectionInfo, timeout)
+	panicIfError(err, "Could not create test PLC!")
+	defer func() {
+		err := device.Close()
+		if err != nil {
+			fmt.Println("Close was unsuccessful:", err.Error())
+		}
+	}()
+
+	http.Handle("/tags/raw", RawTagsHandler{device, knownTags})
+	fmt.Printf("Making PLC '%s' available at '%s'\n", *plcAddr, *httpAddr)
+	err = http.ListenAndServe(*httpAddr, nil)
+	panicIfError(err, "Could not start http server!")
+}
+
+func panicIfError(err error, reason string) {
+	if err != nil {
+		panic("ERROR " + err.Error() + ": " + reason)
+	}
+}
---
+++ cmd/plc-server/server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+
+	"github.com/stellentus/go-plc"
+)
+
+type RawTagsHandler struct {
+	plc.ReadWriter
+	validTags map[string]interface{}
+}
+
+func (h RawTagsHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		h.Read(w, req)
+	case "POST":
+		h.Write(w, req)
+	default:
+		logHTTPError(w, http.StatusBadRequest, "Unexpected verb: "+req.Method)
+	}
+}
+
+func logHTTPError(w http.ResponseWriter, code int, reason string) {
+	log.Printf("Bad http request: %s (%d) - %s\n", http.StatusText(code), code, reason)
+	w.WriteHeader(code)
+	w.Write([]byte(reason))
+}
+
+func copy(v interface{}) interface{} {
+	return reflect.New(reflect.TypeOf(v)).Interface()
+}
+
+func (h RawTagsHandler) Read(w http.ResponseWriter, req *http.Request) {
+	tags := req.FormValue("tags")
+	tagsToFetch := map[string]interface{}{}
+
+	if tags != "" {
+		for _, tag := range strings.Split(tags, ",") {
+			v, ok := h.validTags[tag]
+			if !ok {
+				logHTTPError(w, http.StatusNotFound, "Tag does not exist: "+tag)
+				return
+			}
+			tagsToFetch[tag] = copy(v)
+		}
+	} else {
+		for tag, v := range h.validTags {
+			tagsToFetch[tag] = copy(v)
+		}
+	}
+
+	for tag, value := range tagsToFetch {
+		h.ReadTag(tag, value)
+		tagsToFetch[tag] = value
+	}
+
+	json.NewEncoder(w).Encode(tagsToFetch)
+}
+
+func (h RawTagsHandler) Write(w http.ResponseWriter, req *http.Request) {
+	values := map[string]interface{}{}
+	err := json.NewDecoder(req.Body).Decode(&values)
+
+	switch {
+	case err != nil:
+		logHTTPError(w, http.StatusBadRequest, "failed to decode user error: "+err.Error())
+		return
+	case len(values) == 0:
+		logHTTPError(w, http.StatusBadRequest, "empty list of tags")
+		return
+	default:
+		for tag := range values {
+			_, ok := h.validTags[tag]
+			if !ok {
+				logHTTPError(w, http.StatusNotFound, "Tag does not exist: "+tag)
+				return
+			}
+		}
+	}
+
+	var lastError error
+	for tag, value := range values {
+		err := h.WriteTag(tag, value)
+		if err != nil {
+			lastError = err
+			values[tag] = err
+		}
+	}
+
+	if lastError != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	json.NewEncoder(w).Encode(values)
+}
