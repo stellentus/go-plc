18111c720b5ed60d513492b7386798749e5ed5ed
James Bell <james@bellstone.ca>
2020-11-24 12:42:12 -0700

    Update example/cache to do its own building

--- example/cache/main.go
+++ example/cache/main.go
@@ -5,52 +5,73 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/stellentus/go-plc"
 	"github.com/stellentus/go-plc/example"
 )
 
-var addr = flag.String("address", "192.168.1.176", "Hostname or IP address of the PLC")
-var numWorkers = flag.Int("workers", 1, "Number of worker threads talking to libplctag")
-var refreshDuration = flag.Duration("refresh", time.Second, "Refresh period")
-var tagName = flag.String("tagName", "DUMMY_AQUA_DATA_0[0]", "Name of the uint8 tag to read repeatedly")
+var (
+	addr       = flag.String("address", "192.168.1.176", "Hostname or IP address of the PLC")
+	numWorkers = flag.Int("workers", 1, "Number of worker threads talking to libplctag")
+	tagName    = flag.String("tagName", "DUMMY_AQUA_DATA_0[0]", "Name of the uint8 tag to read repeatedly")
+)
 
 // This command demonstrates setting up to read and write values from a plant.
 func main() {
 	flag.Parse()
 
-	dev, err := example.NewDevice(example.Config{
-		Workers:          *numWorkers,
-		PrintIODebug:     true,
-		DebugFunc:        fmt.Printf,
-		DeviceConnection: map[string]string{"gateway": *addr},
-		UseCache:         true,
-	})
-	if err != nil {
-		panic("ERROR " + err.Error() + ": Could not create test PLC!")
-	}
+	fmt.Printf("Initializing connection to %s\n", *addr)
+
+	device, err := plc.NewDevice(map[string]string{"gateway": *addr})
+	panicIfError(err, "Could not create test PLC!")
 	defer func() {
-		err := dev.Close()
-		if err != nil {
-			panic("ERROR: Close was unsuccessful:" + err.Error())
-		}
+		err := device.Close()
+		panicIfError(err, "Close was unsuccessful")
 	}()
 
-	cache := dev.Cache()
+	// Wrap with debug
+	rw := plc.ReadWriter(example.DebugPrinter{
+		ReadPrefix: "READ",
+		Reader:     device,
+		Writer:     device,
+		DebugFunc:  fmt.Printf,
+	})
+
+	if *numWorkers > 0 {
+		fmt.Printf("Creating a pool of %d threads\n", *numWorkers)
+		rw = plc.NewPooled(rw, *numWorkers)
+	}
+
+	fmt.Printf("Creating a cache\n")
+	directReader := plc.NewCache(rw) // Reads the device directly
+
+	// cacheReader only reads out of the cache
+	cacheReader := example.DebugPrinter{
+		ReadPrefix: "CACHE-READ",
+		Reader:     directReader.CacheReader(),
+		DebugFunc:  fmt.Printf,
+	}
 
 	// Get the first read
 	val := uint8(0)
 	original := uint8(0)
-	dev.ReadTag(*tagName, &original)
-	cache.ReadTag(*tagName, &val)
+	directReader.ReadTag(*tagName, &original)
+	cacheReader.ReadTag(*tagName, &val)
 
 	// Now write a new value, but re-read from the cache
-	dev.WriteTag(*tagName, val+1)
+	rw.WriteTag(*tagName, val+1)
 	time.Sleep(200 * time.Millisecond) // Arbitrary time to make sure the write completed
-	cache.ReadTag(*tagName, &val)
+	cacheReader.ReadTag(*tagName, &val)
 
 	// Now read the value and show the updated cache
-	dev.ReadTag(*tagName, &val)
-	cache.ReadTag(*tagName, &val)
+	directReader.ReadTag(*tagName, &val)
+	cacheReader.ReadTag(*tagName, &val)
 
 	// Now return to the original value.
-	dev.WriteTag(*tagName, original)
+	rw.WriteTag(*tagName, original)
+}
+
+func panicIfError(err error, reason string) {
+	if err != nil {
+		panic("ERROR " + err.Error() + ": " + reason)
+	}
 }
