299fd807212823bfc562e2b00a7c995955d8be37
James Bell <james@bellstone.ca>
2020-11-26 13:06:18 -0700

    Remove index flag from example/toggle-bool

--- example/toggle-bool/main.go
+++ example/toggle-bool/main.go
@@ -11,7 +11,6 @@ var (
 	addr     = flag.String("address", "192.168.1.176", "Hostname or IP address of the PLC")
 	path     = flag.String("path", "1,0", "Path to the PLC at the provided host or IP")
 	tagName  = flag.String("tagName", "DUMMY_AQUA_DATA_0[0]", "Name of the boolean tag to toggle")
-	index    = flag.Int("index", -1, "Array index to access, or -1 if not an array")
 	plcDebug = flag.Int("plctagdebug", 0, "Debug level for libplctag's debug (0-5)")
 )
 
@@ -40,11 +39,7 @@ func main() {
 
 	// Read. If non-zero, value is true. Otherwise, it's false.
 	var isOn bool
-	if *index >= 0 {
-		err = device.ReadTag(plc.TagWithIndex(*tagName, *index), &isOn)
-	} else {
-		err = device.ReadTag(*tagName, &isOn)
-	}
+	err = device.ReadTag(*tagName, &isOn)
 	if err != nil {
 		panic("ERROR: Unable to read the data because " + err.Error())
 	}
@@ -52,22 +47,14 @@ func main() {
 
 	// Toggle the bool state
 	isOn = !isOn
-	if *index >= 0 {
-		err = device.WriteTag(plc.TagWithIndex(*tagName, *index), isOn)
-	} else {
-		err = device.WriteTag(*tagName, isOn)
-	}
+	err = device.WriteTag(*tagName, isOn)
 	if err != nil {
 		panic("ERROR: Unable to write the data because " + err.Error())
 	}
 
 	// Confirm that it was toggled as expected
 	var newIsOn bool
-	if *index >= 0 {
-		err = device.ReadTag(plc.TagWithIndex(*tagName, *index), &newIsOn)
-	} else {
-		err = device.ReadTag(*tagName, &newIsOn)
-	}
+	err = device.ReadTag(*tagName, &newIsOn)
 	if err != nil {
 		panic("ERROR: Unable to read the data because " + err.Error())
 	}
