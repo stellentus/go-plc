d649907a7fd80278c58dff5e0e9f552c2ae891b7
James Bell <james@bellstone.ca>
2020-11-26 13:05:07 -0700

    Create example/read-at-index

---
+++ example/read-at-index/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+
+	"github.com/stellentus/go-plc"
+)
+
+var (
+	addr     = flag.String("address", "192.168.1.176", "Hostname or IP address of the PLC")
+	tagName  = flag.String("tagName", "DUMMY_ARRAY_TAG", "Name of the array tag to read")
+	index    = flag.Int("index", 0, "Array index to access")
+	plcDebug = flag.Int("plctagdebug", 0, "Debug level for libplctag's debug (0-5)")
+)
+
+func main() {
+	flag.Parse()
+	if *index < 0 {
+		panic("Cannot access negative array index: " + strconv.Itoa(*index))
+	}
+
+	plc.SetLibplctagDebug(plc.LibplctagDebugLevel(*plcDebug))
+
+	fmt.Printf("Initializing connection to %s using %s\n", *addr, *tagName)
+
+	device, err := plc.NewDevice(map[string]string{"gateway": *addr})
+	if err != nil {
+		panic("ERROR " + err.Error() + ": Could not create test PLC!")
+	}
+	defer func() {
+		err := device.Close()
+		if err != nil {
+			fmt.Println("Close was unsuccessful:", err.Error())
+		}
+	}()
+
+	var tagValue uint32
+	err = device.ReadTag(plc.TagWithIndex(*tagName, *index), &tagValue)
+	if err != nil {
+		panic("ERROR: Unable to read the data because " + err.Error())
+	}
+	fmt.Printf("%s[%d] is %v\n", *tagName, *index, tagValue)
+}
