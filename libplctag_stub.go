3924fb752a138ae875934fc458ab07154314316e
James Bell <james@bellstone.ca>
2020-11-27 12:29:05 -0700

    Register the stub tag type

--- libplctag_stub.go
+++ libplctag_stub.go
@@ -7,3 +7,11 @@ package plc
 #include <libplctag.h>
 */
 import "C"
+
+const dummyTagType = 0x2000
+
+func init() {
+	// The stub uses type 0x2000 for everything. The pre-loaded ones are uint16.
+	RegisterTagTypeName(dummyTagType, "dummy_type")
+	RegisterTagType(dummyTagType, uint16(0))
+}
