326aaef25084cf5d1090f7d12972ba7ca1ad2cf0
James Bell <james@bellstone.ca>
2020-12-02 14:57:52 -0700

    Provide a way to get errors from Refresher (#6)

--- refresher.go
+++ refresher.go
@@ -20,6 +20,11 @@ type Refresher struct {
 	period time.Duration
 	seen   map[string]struct{}
 	mutex  sync.Mutex
+
+	// ErrorCallback is called if an error is encountered while refreshing.
+	// If no callback is set, the error is silently discarded (and you're a bad
+	// person for not handling your errors 😜).
+	ErrorCallback func(error)
 }
 
 var _ = Reader(&Refresher{}) // Compiler makes sure this type is a Reader
@@ -51,7 +56,12 @@ func (r *Refresher) launchIfNecessary(name string, value interface{}, f func(v i
 }
 
 func (r *Refresher) ReadTag(name string, value interface{}) error {
-	r.launchIfNecessary(name, value, func(v interface{}) { r.plc.ReadTag(name, v) })
+	r.launchIfNecessary(name, value, func(v interface{}) {
+		err := r.plc.ReadTag(name, v)
+		if err != nil && r.ErrorCallback != nil {
+			r.ErrorCallback(err)
+		}
+	})
 
 	return r.plc.ReadTag(name, value)
 }
