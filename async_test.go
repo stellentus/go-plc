5d1ed3451d24037a1ccaf66c51bc06b822ce649f
James Bell <james@bellstone.ca>
2021-02-23 14:45:38 -0700

    Add tests to ensure async is parallel

--- async_test.go
+++ async_test.go
@@ -2,10 +2,11 @@ package plc
 
 import (
 	"errors"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestAsyncAddError(t *testing.T) {
@@ -33,11 +34,84 @@ func TestNewAsync(t *testing.T) {
 	value := int(8)
 	expErr := errors.New("Test")
 	as := newAsync(func(nm string, val interface{}) error {
-		require.Equal(t, name, nm, "Async should pass the name unchanged")
-		require.Equal(t, value, val, "Async should pass the value unchanged")
+		assert.Equal(t, name, nm, "Async should pass the name unchanged")
+		assert.Equal(t, value, val, "Async should pass the value unchanged")
 		return expErr
 	})
 	as.Add(name, value)
 	err := as.Wait()
-	require.Equal(t, expErr, err, "Async should pass the error unchanged")
+	assert.Equal(t, expErr, err, "Async should pass the error unchanged")
+}
+
+func TestAsyncMany(t *testing.T) {
+	sendVals := make([]int, 8)
+	receiveVals := make([]int, 8)
+	for i := range sendVals {
+		sendVals[i] = i
+		receiveVals[i] = -1
+	}
+
+	as := newAsync(func(nm string, val interface{}) error {
+		idx, err := strconv.Atoi(nm)
+		receiveVals[idx] = val.(int)
+		return err
+	})
+
+	for i := range sendVals {
+		as.Add(strconv.Itoa(i), sendVals[i])
+	}
+
+	err := as.Wait()
+	assert.NoError(t, err)
+	assert.Equal(t, sendVals, receiveVals)
+}
+
+func TestAsyncManyParallel(t *testing.T) {
+	numToTest := 8
+	sendVals := make([]int, numToTest)
+	receiveVals := make([]int, numToTest)
+	for i := range sendVals {
+		sendVals[i] = i
+		receiveVals[i] = -1
+	}
+
+	done := make(chan struct{})
+	count := make(chan int, numToTest)
+
+	as := newAsync(func(nm string, val interface{}) error {
+		idx, err := strconv.Atoi(nm)
+		count <- idx // indicate this index executed
+		receiveVals[idx] = val.(int)
+
+		<-done // Block until this channel is closed
+		return err
+	})
+
+	for i := range sendVals {
+		as.Add(strconv.Itoa(i), sendVals[i])
+	}
+
+	receivedIndices := uint32(0) // this limits the max number of parallel tests to 32
+	numReceived := 0
+
+outerLoop:
+	for {
+		select {
+		case <-time.After(50 * time.Millisecond):
+			assert.Fail(t, "Timeout in reading from parallel async")
+			break outerLoop
+		case idx := <-count:
+			receivedIndices |= 1 << uint32(idx)
+			numReceived++
+			if numReceived >= numToTest {
+				break outerLoop
+			}
+		}
+	}
+	close(done)
+
+	err := as.Wait()
+	assert.Equal(t, uint32(1<<numToTest)-1, receivedIndices, "Not all expected async occurred in parallel")
+	assert.NoError(t, err)
+	assert.Equal(t, sendVals, receiveVals)
 }
